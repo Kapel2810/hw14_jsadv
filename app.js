da7c017451d977c23451b9b4d5936be3588cf1d5
Katrina Apel <kapel2810@gmail.com>
2021-12-08 10:19:30 +0200

    ex2 done

--- app.js
+++ app.js
@@ -1,23 +1,42 @@
- 
+
 //////////////////////////////
 //------------- ex1-------------
 
-let emailGroup = new Set()
+// let emailGroup = new Set()
 
-emailGroup.add('ronen@mail.com')
-emailGroup.add('malka@mail.com')
-emailGroup.add('adi@mail.com')
-emailGroup.add('darian@mail.com')
-emailGroup.add('moti@mail.com')
+// emailGroup.add('ronen@mail.com')
+// emailGroup.add('malka@mail.com')
+// emailGroup.add('adi@mail.com')
+// emailGroup.add('darian@mail.com')
+// emailGroup.add('moti@mail.com')
 
- const runCode = ()=> {
-    for (let email of emailGroup){
-        console.log(email)
-    }
-    if (emailGroup.has('moishe@gmail.com')){
-        console.log('great')
-    }else {
-        console.log('there is no such email in email group')
-    }
+//  const runCode = ()=> {
+//     for (let email of emailGroup){
+//         console.log(email)
+//     }
+//     if (emailGroup.has('moishe@gmail.com')){
+//         console.log('great')
+//     }else {
+//         console.log('there is no such email in email group')
+//     }
 
- }
+//  }
+
+//////////////////////////////////////////
+//-------------ex2---------------
+
+let userNameList = new Set()
+
+
+userNameList.add(prompt('please enter your username'))
+userNameList.add(prompt('please enter your username'))
+userNameList.add(prompt('please enter your username'))
+userNameList.add(prompt('please enter your username'))
+userNameList.add(prompt('please enter your username'))
+
+const runCode = ( )=> {
+    for(let userName of userNameList){
+        console.log(userName)
+    } 
+    console.log(userNameList.size)
+}
